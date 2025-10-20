db5a646b84ed55764810bd6a275545aec5e9e8f3
AdianCotto <adian.cotto1@gmail.com>
2025-10-19 21:17:32 -0400

    Add /message route that reads MESSAGE env var

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => res.send("Hello from Heroku!"));
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+app.get("/message", (_req, res) => {
+  const msg = process.env.MESSAGE || "No MESSAGE set";
+  res.send(`MESSAGE: ${msg}`);
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
